137903bc1ed3e4ebf943c6122d5e26f4520dbabc
Wafia Alsheikh <vafyeelseyh@yahoo.com>
2021-12-12 16:12:43 +0200

    Created detailedlist_service.dart

---
+++ UHave/lib/services/detailedlist_service.dart
@@ -0,0 +1,33 @@
+import 'package:uhave_project/modules/detailedList.dart';
+import 'package:uhave_project/repository/detailedlist_repository.dart';
+
+class DetailedListService{
+  late DetailedListRepository _respository;
+
+  DetailedListService(){
+    _respository = DetailedListRepository();
+  }
+  // creating data
+  saveDetailedList(detailedList list) async{
+    return await _respository.insertData('detailedList', list.categoryMap());
+  }
+
+  readDetailedList(categoryId,tarih) async{
+    return await _respository.readData('detailedList',categoryId,tarih);
+  }
+
+  // read data from table by id
+  readDetailedListById(itemId) async {
+    return await _respository.readDataById('detailedList',itemId);
+  }
+
+  // update data from table
+  updateDetailedList(detailedList list) async {
+    return await _respository.updateData('detailedList', list.categoryMap());
+  }
+
+  // delete category from database
+  deleteDetailedList(itemId) async{
+    return await _respository.deleteData('detailedList',itemId);
+  }
+}
