5ad6c5fb51b0cdb0ebb86a8609c397dc6f4082a1
Wafia Alsheikh <vafyeelseyh@yahoo.com>
2021-12-12 16:09:54 +0200

    Updated category.dart

--- UHave/lib/modules/category.dart
+++ UHave/lib/modules/category.dart
@@ -1,36 +1,12 @@
-final String tableCategory='category';
+class Category {
+    int? id;
+    String? name;
 
-class categoryFields{
-  static final List<String> values = [
-    /// Add all fields
-    category_id, category_name
-  ];
-  static final String category_id = '_category_id';
-  static final String category_name = 'category_name';
-}
+  categoryMap(){
+    var mapping = Map<String, dynamic>();
+    mapping['id'] = id;
+    mapping['name']= name;
 
-class category{
-  final int? category_id;
-  late final String category_name;
-
-   category({
-    this.category_id,
-    required this.category_name
-  });
-  category Copy({
-    int? category_id,
-    String? category_name
-  }) =>
-      category(
-          category_id: category_id ?? this.category_id,
-          category_name: category_name ?? this.category_name
-      );
-  static category fromJson(Map<String, Object?>json) => category(
-    category_id: json[categoryFields.category_id] as int?,
-    category_name: json[categoryFields.category_name] as String,
-  );
-  Map<String, Object?> toJson() => {
-    categoryFields.category_id : category_id,
-    categoryFields.category_name: category_id
-  };
+    return mapping;
+  }
 }
