7412d09c34c34a89a1fda610174460c9196262f8
YusaCan <ahmedyusacan@gmail.com>
2021-12-21 20:40:54 +0300

    Update main.dart

--- UHave/lib/main.dart
+++ UHave/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:provider/provider.dart';
 import 'package:uhave_project/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:uhave_project/services/notification.dart';
 
 import 'Calendar.dart';
 
@@ -10,12 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    /*  return MaterialApp(
       title: "calendar",
       home: HomePage(),
       //routes: {'/calendar': (_) => Calendar()},
       //bu örnekte static veriler sadece transfer edilir sayfalar arasında
       //onGenerateRoute: (settings){},
+    ); */
+
+    return MultiProvider(
+      child: MaterialApp(
+        theme: ThemeData(fontFamily: 'Monteserat'),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+      providers: [ChangeNotifierProvider(create: (_) => NotificationService())],
     );
   }
 }
