8f92f13e4c1bd93eb559ab20e7616d2e6a645aa0
YusaCan <ahmedyusacan@gmail.com>
2021-12-24 20:21:19 +0300

    Update main.dart

--- UHave/lib/main.dart
+++ UHave/lib/main.dart
@@ -12,14 +12,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /*  return MaterialApp(
-      title: "calendar",
-      home: HomePage(),
-      //routes: {'/calendar': (_) => Calendar()},
-      //bu örnekte static veriler sadece transfer edilir sayfalar arasında
-      //onGenerateRoute: (settings){},
-    ); */
-
     return MultiProvider(
       child: MaterialApp(
         theme: ThemeData(fontFamily: 'Monteserat'),
