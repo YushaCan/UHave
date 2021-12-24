bceb66c4faf41acd3fc88f3e1c070502bde1b083
YusaCan <ahmedyusacan@gmail.com>
2021-12-24 20:21:18 +0300

    Update detailedlist_repository.dart

--- UHave/lib/repository/detailedlist_repository.dart
+++ UHave/lib/repository/detailedlist_repository.dart
@@ -1,35 +1,37 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:uhave_project/repository/database_connection.dart';
 
-class DetailedListRepository{
+class DetailedListRepository {
   late DatabaseConnection _databaseConnection;
 
-  DetailedListRepository(){
+  DetailedListRepository() {
     _databaseConnection = DatabaseConnection();
   }
   Database? _database;
   //to get the connection with the database
-  Future<Database?> get database async{
-    if(_database!=null) return _database;
+  Future<Database?> get database async {
+    if (_database != null) return _database;
     _database = await _databaseConnection.setDatabase();
     return _database;
   }
 
   //to insert values to the table in the given database
-  insertData(table, data) async{
+  insertData(table, data) async {
     var connection = await database;
     return await connection!.insert(table, data);
   }
 
   //read data from table
-  readAllData(table, categoryId) async{
+  readAllData(table, categoryId) async {
     var connection = await database;
-    return await connection!.query(table, where: 'categoryId=?', whereArgs: [categoryId]);
+    return await connection!
+        .query(table, where: 'categoryId=?', whereArgs: [categoryId]);
   }
 
-  readData(table, categoryId, tarih) async{
+  readData(table, categoryId, tarih) async {
     var connection = await database;
-    return await connection!.query(table, where: 'categoryId=? and tarih=?', whereArgs: [categoryId,tarih]);
+    return await connection!.query(table,
+        where: 'categoryId=? and tarih=?', whereArgs: [categoryId, tarih]);
   }
 
   // read data from table by id
@@ -39,13 +41,14 @@ class DetailedListRepository{
   }
 
   // update data from table
-  updateData(table, data) async{
+  updateData(table, data) async {
     var connection = await database;
-    return await connection!.update(table, data, where: 'id=?',whereArgs: [data['id']]);
+    return await connection!
+        .update(table, data, where: 'id=?', whereArgs: [data['id']]);
   }
 
   // delete data from table
-  deleteData(table,itemId) async{
+  deleteData(table, itemId) async {
     var connection = await database;
     return await connection!.rawDelete("Delete from $table where id=$itemId");
   }
