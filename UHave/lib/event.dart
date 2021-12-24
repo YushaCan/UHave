e4f432ab7fc2ee2993dc16fb2b8d87951509f03f
YusaCan <ahmedyusacan@gmail.com>
2021-12-24 20:21:11 +0300

    Delete event.dart

--- UHave/lib/event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:uhave_project/DetailedList.dart';
-
-class Event {
-  final String title;
-  Event({required this.title});
-  String toString() => this.title;
-}
